26cd5b5e8a3085ed661edcbf5962d36bf2f352c8
@hexlet/cli <support@hexlet.io>
2023-11-17 17:36:34 +0600

    submit java-spring-ru/beans

---
+++ java-spring-ru/beans/src/main/java/exercise/daytime/Night.java
@@ -0,0 +1,17 @@
+package exercise.daytime;
+import jakarta.annotation.PostConstruct;
+
+public class Night implements Daytime {
+    private String name = "night";
+
+    public String getName() {
+        return name;
+    }
+
+    // BEGIN
+    @PostConstruct
+    public void init() {
+        System.out.println("Night class is initialized!");
+    }
+    // END
+}
