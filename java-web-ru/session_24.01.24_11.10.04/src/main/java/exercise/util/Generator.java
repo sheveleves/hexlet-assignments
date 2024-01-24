7b1910255c0ba181719a88c913586ab969344d23
@hexlet/cli <support@hexlet.io>
2024-01-24 11:18:18 +0600

    submit java-web-ru/session_24.01.24_11.10.04

---
+++ java-web-ru/session_24.01.24_11.10.04/src/main/java/exercise/util/Generator.java
@@ -0,0 +1,18 @@
+package exercise.util;
+
+import java.util.List;
+import java.util.ArrayList;
+import exercise.model.User;
+
+public class Generator {
+
+    public static List<User> getUsers() {
+        List<User> users = new ArrayList<>();
+
+        users.add(new User(1L, "admin", Security.encrypt("secret")));
+        users.add(new User(2L, "mike", Security.encrypt("superpass")));
+        users.add(new User(3L, "kate", Security.encrypt("strongpass")));
+
+        return users;
+    }
+}
