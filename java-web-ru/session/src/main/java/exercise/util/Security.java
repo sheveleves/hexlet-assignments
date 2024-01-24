59bee317e62c3f33c36f494f9339e80e6e330d86
@hexlet/cli <support@hexlet.io>
2024-01-24 12:16:37 +0600

    submit java-web-ru/session

--- java-web-ru/session/src/main/java/exercise/util/Security.java
+++ java-web-ru/session/src/main/java/exercise/util/Security.java
@@ -1,25 +1,13 @@
 package exercise.util;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import com.google.common.hash.Hashing;
+import java.nio.charset.StandardCharsets;
 
 public class Security {
     public static String encrypt(String password) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] passwordBytes = password.getBytes();
-            byte[] hashedBytes = md.digest(passwordBytes);
-
-            StringBuilder sb = new StringBuilder();
-            for (byte b : hashedBytes) {
-                sb.append(String.format("%02x", b));
-            }
-
-            return sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
-        return null;
+        var hashed = Hashing.sha256()
+            .hashString(password, StandardCharsets.UTF_8)
+            .toString();
+        return hashed;
     }
 }
