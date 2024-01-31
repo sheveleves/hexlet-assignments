7b7b44d25c1e5384b62b508426bf2adc42de44ac
@hexlet/cli <support@hexlet.io>
2024-01-31 18:33:27 +0600

    submit java-web-ru/database

---
+++ java-web-ru/database/src/main/java/exercise/repository/ProductsRepository.java
@@ -0,0 +1,64 @@
+package exercise.repository;
+
+import java.sql.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import exercise.model.Product;
+
+public class ProductsRepository extends BaseRepository {
+
+    // BEGIN
+    public static void save(Product product) throws SQLException {
+        String sql = "INSERT INTO product (title, price) VALUES (?, ?)";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, product.getTitle());
+            preparedStatement.setInt(2, product.getPrice());
+            preparedStatement.executeUpdate();
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                product.setId(generatedKeys.getLong(1));
+            } else {
+                throw new SQLException("DB have not returned an id after saving an entity");
+            }
+        }
+    }
+
+    public static Optional<Product> find(Long id) throws SQLException {
+        String sql = "SELECT * FROM product WHERE id = ?";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                String title = resultSet.getString("title");
+                int price = resultSet.getInt("price");
+                Product product = new Product(title, price);
+                product.setId(id);
+                return Optional.of(product);
+            }
+            return Optional.empty();
+        }
+    }
+
+    public static List<Product> getEntities() throws SQLException {
+        String sql = "SELECT * FROM product";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Product> products = new ArrayList<Product>();
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String title = resultSet.getString("title");
+                int price = resultSet.getInt("price");
+                Product product = new Product(title, price);
+                product.setId(id);
+                products.add(product);
+            }
+            return products;
+        }
+    }
+    // END
+}
---
+++ java-web-ru/database/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+-- BEGIN
+DROP TABLE IF EXISTS product;
+
+CREATE TABLE product (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    price INT
+);
+-- END
